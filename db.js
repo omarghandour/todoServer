7baba2db26799150ccb7d9d787df35841778eb22
omarghandour <109479370+omarghandour@users.noreply.github.com>
2023-08-28 00:56:04 +0300

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const pool = new Pool({
     user: process.env.USER,
     password: process.env.PASSWORD,
-    host=process.env.POSTGRES_HOST,
+    host:process.env.POSTGRES_HOST,
     connectionString: process.env.POSTGRES_URL + "?sslmode=require",
     port: process.env.DBPORT,
     database: "verceldb"
